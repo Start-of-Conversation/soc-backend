571f773440de89ab445d0ca4a103c91965827192
ahma0 <dsd932@naver.com>
2024-09-22 16:58:22 +0900

    chore: project setting

---
+++ src/main/kotlin/toyproject/web/startofconversation/StartOfConversationApplication.kt
@@ -0,0 +1,11 @@
+package toyproject.web.startofconversation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StartOfConversationApplication
+
+fun main(args: Array<String>) {
+    runApplication<StartOfConversationApplication>(*args)
+}
---
+++ src/test/kotlin/toyproject/web/startofconversation/StartOfConversationApplicationTests.kt
@@ -0,0 +1,13 @@
+package toyproject.web.startofconversation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartOfConversationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
