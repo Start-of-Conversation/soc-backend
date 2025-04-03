9902fb68fdffdd9902f02d7bedb5dcce0c3bfe05
ahma0 <dsd932@naver.com>
2025-04-03 17:40:45 +0900

    chore: Add dependency 'auth'

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(project(":common"))
+    implementation(project(":auth"))
     implementation("org.springframework.security:spring-security-crypto")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
