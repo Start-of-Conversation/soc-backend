c929c17e0db91a3af9fa78a6fc6f39f634f1ae87
ahma0 <dsd932@naver.com>
2025-06-01 20:55:29 +0900

    feat: set default values

--- common/src/main/kotlin/toyproject/startofconversation/common/domain/cardgroup/exception/CardGroupNotFoundException.kt
+++ common/src/main/kotlin/toyproject/startofconversation/common/domain/cardgroup/exception/CardGroupNotFoundException.kt
@@ -3,5 +3,5 @@ package toyproject.startofconversation.common.domain.cardgroup.exception
 import toyproject.startofconversation.common.exception.SOCNotFoundException
 
 class CardGroupNotFoundException(
-    id: String, cause: Throwable?
+    id: String, cause: Throwable? = null
 ) : SOCNotFoundException("CardGroup $id not found.", cause)
--- common/src/main/kotlin/toyproject/startofconversation/common/domain/cardgroup/exception/MaxGroupCapacityExceededException.kt
+++ common/src/main/kotlin/toyproject/startofconversation/common/domain/cardgroup/exception/MaxGroupCapacityExceededException.kt
@@ -3,5 +3,5 @@ package toyproject.startofconversation.common.domain.cardgroup.exception
 import toyproject.startofconversation.common.exception.SOCNotFoundException
 
 class MaxGroupCapacityExceededException(
-    maxSize: Int, cause: Throwable?
+    maxSize: Int, cause: Throwable? = null
 ) : SOCNotFoundException("Group can only have up to $maxSize members.", cause)
