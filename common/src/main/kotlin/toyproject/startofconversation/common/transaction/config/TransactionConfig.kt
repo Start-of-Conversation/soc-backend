b53d0dc3599db231695c2b1d41176476c06f795d
ahma0 <dsd932@naver.com>
2025-06-05 22:46:20 +0900

    feat: add transactional logic to ensure data consistency

---
+++ common/src/main/kotlin/toyproject/startofconversation/common/transaction/config/TransactionConfig.kt
@@ -0,0 +1,15 @@
+package toyproject.startofconversation.common.transaction.config
+
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import toyproject.startofconversation.common.transaction.helper.Tx
+import toyproject.startofconversation.common.transaction.runner.TransactionRunner
+
+@Configuration
+class TransactionConfig {
+
+    @Bean
+    fun txInitialize(txRunner: TransactionRunner): InitializingBean =
+        InitializingBean { Tx.initialize(txRunner) }
+}
