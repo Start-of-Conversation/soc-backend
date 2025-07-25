4f53f186bc83068a2c503292f1c12a5c6749b5fd
ahma0 <dsd932@naver.com>
2025-07-25 19:08:52 +0900

    chore: add AWS S3 dependency and property

---
+++ api/src/main/kotlin/toyproject/startofconversation/api/image/config/S3Config.kt
@@ -0,0 +1,22 @@
+package toyproject.startofconversation.api.image.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+
+@Profile("prod")
+@Configuration
+class S3Config {
+
+    @Value("\${aws.secrets-manager.region}")
+    private lateinit var region: String
+
+    @Bean
+    fun s3Client(): S3Client = S3Client.builder()
+        .region(Region.of(region))
+        .build()
+
+}
